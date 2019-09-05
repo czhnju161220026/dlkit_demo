25a67a44d7b6b7cee31ffb43465e1246c9a32f78
czhnju161220026 <997520420@qq.com>
2019-09-05 10:58:00 +0800

    'update'

---
+++ launcher.py
@@ -0,0 +1,22 @@
+# encoding=utf-8
+
+from pytorch_demos.demo1.main import main as pt_m1
+from datetime import datetime
+
+tasks = {
+    'Pytorch Minist' : pt_m1
+}
+
+def launch(func_name):
+    func = tasks[func_name]
+    start = datetime.now()
+    print('{:-^60s}'.format(func_name))
+    print('%s starts executing at %s' % (func_name.ljust(20), start))
+    func()
+    elapsed = datetime.now() - start
+    print('%s finished, Elapsed time:%s' % (func_name.ljust(20), elapsed))
+
+
+if __name__ == '__main__':
+    for task in tasks:
+        launch(task)
